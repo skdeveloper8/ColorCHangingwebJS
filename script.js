12579bf9c8618d1ad388b7ba1cb94507d2062e98
Sangeet Kumar <91060606+skdeveloper8@users.noreply.github.com>
2023-07-16 11:21:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const buttons=document.querySelectorAll('.button')
+// console.log(buttons);
+const body=document.querySelector("body")
+
+
+buttons.forEach(function (button){
+   console.log(button);
+
+   button.addEventListener('click',function(e){
+           console.log(e);
+           console.log(e.target);
+
+          if(e.target.id==='blue'){
+            body.style.background=e.target.id
+          }
+          if(e.target.id==='orange'){
+            body.style.background=e.target.id
+          }  
+          if(e.target.id==='pink'){
+            body.style.background=e.target.id
+          } 
+           if(e.target.id==='red'){
+            body.style.background=e.target.id
+          } 
+           if(e.target.id==='green'){
+            body.style.background=e.target.id
+          }  
+   })
+})
